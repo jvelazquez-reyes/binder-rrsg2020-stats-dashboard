c2387324f60dc4c715f73c117748b60209068780
Juan Velazquez <velazquezrj2012@licifug.ugto.mx>
2021-06-02 17:17:51 -0500

    Revert commits for HSF

--- ShinyDashboard/app.R
+++ ShinyDashboard/app.R
@@ -22,12 +22,10 @@ ui <- navbarPage("T1 mapping challenge statistics", theme = shinytheme("flatly")
                  #TAB 1
                  tabPanel("Overview",
                           fluidPage(
-                              htmlOutput("overviewTxt1"),
-                              mainPanel(img(src='challengePitch.png', height="130%", width="130%", align = "center"))
+                              htmlOutput("overviewTxt1")
                           ),
                           fluidPage(
-                              htmlOutput("overviewTxt2"),
-                              mainPanel(img(src='rrsg2020Repo.png', height="90%", width="90%", align = "center"))
+                              htmlOutput("overviewTxt2")
                           ),
                           fluidPage(
                               htmlOutput("overviewTxt3")
@@ -155,32 +153,28 @@ ui <- navbarPage("T1 mapping challenge statistics", theme = shinytheme("flatly")
                                   plotlyOutput(outputId = "AcErrorAllPoints")
                               )
                           )
+                          ),
+                          
+                          tabsetPanel(sidebarLayout(
+                              sidebarPanel(
+                                  selectizeInput(
+                                      inputId = "DispHSF", 
+                                      label = "Select a sphere", 
+                                      choices = unique(MeasSites$dataSite_long$sph_long),
+                                      multiple = FALSE)
+                              ),
+                              
+                              mainPanel(
+                                  h3("Hierarchical shift function analysis"),
+                                  plotlyOutput(outputId = "DecilesDiff"),
+                                  plotlyOutput(outputId = "BootstrapDiff"),
+                                  plotlyOutput(outputId = "BootstrapDensities")
+                              )
+                          )
                           )
                  ),
                  
                  #TAB 4
-                 tabPanel("HSF",
-                          tabsetPanel(sidebarLayout(
-                              sidebarPanel(width = 0.5),
-                              mainPanel(
-                                  h3("Bootstrapped Differences"),
-                                  plotOutput(outputId = "BootstrapDiff", height = 1600, width = 1600),
-                                  #plotOutput(outputId = "BootstrapDensities")
-                              )
-                          )
-                          ),
-                          
-                          tabsetPanel(sidebarLayout(
-                              sidebarPanel(width = 0.5),
-                              mainPanel(
-                                  h3("Decile Differences"),
-                                  plotlyOutput(outputId = "DecilesDiff", height = 1600, width = 1600)
-                              )
-                          )
-                          )
-                 ),
-                 
-                 #TAB 5
                  navbarMenu("Measured VS Reference T1",
                             tabPanel("Site",
                                      sidebarLayout(
@@ -236,7 +230,7 @@ ui <- navbarPage("T1 mapping challenge statistics", theme = shinytheme("flatly")
                             )
                  ),
                  
-                 #TAB 6
+                 #TAB 5
                  tabPanel("Standard Deviation",
                           tabsetPanel(sidebarLayout(
                               sidebarPanel(
@@ -258,7 +252,7 @@ ui <- navbarPage("T1 mapping challenge statistics", theme = shinytheme("flatly")
                           )
                 )
                 
-                #TAB 7
+                #TAB 6
                 #tabPanel("LMEM",
                 #         sidebarLayout(
                 #             sidebarPanel(
@@ -537,69 +531,179 @@ server <- function(input, output) {
         CorrTableSites$corrSph_across_sites
     })
     
-    #TAB 4
     #################HSF#####################
-    HSFData <- hierarchical_shift_function(dataSites)
-    output$BootstrapDiff <- renderPlot({
-        #HSFData <- hierarchical_shift_function(dataSites, input$DispHSF)
-        g14 <- HSFData$diffBootstrapDiff[[14]]
-        g13 <- HSFData$diffBootstrapDiff[[13]]
-        g12 <- HSFData$diffBootstrapDiff[[12]]
-        g11 <- HSFData$diffBootstrapDiff[[11]]
-        g10 <- HSFData$diffBootstrapDiff[[10]]
-        g9 <- HSFData$diffBootstrapDiff[[9]]
-        g8 <- HSFData$diffBootstrapDiff[[8]]
-        g7 <- HSFData$diffBootstrapDiff[[7]]
-        g6 <- HSFData$diffBootstrapDiff[[6]]
-        g5 <- HSFData$diffBootstrapDiff[[5]]
-        g4 <- HSFData$diffBootstrapDiff[[4]]
-        g3 <- HSFData$diffBootstrapDiff[[3]]
-        g2 <- HSFData$diffBootstrapDiff[[2]]
-        g1 <- HSFData$diffBootstrapDiff[[1]]
-        gs = list(g14,g13,g12,g11,g10,g9,g8,g7,g6,g5,g4,g3,g2,g1)
-        gridExtra::grid.arrange(grobs=gs, ncol = 4)
-    })
+    a = unique(MeasSites$dataSite_long$sid_long)
+    listdat1 = list()
+    listdat2 = list()
+    lengthDAT = array()
+    
+    # analysis parameters
+    np = 20
+    qseq <- seq(0.1,0.9,0.1) # quantiles
+    alpha <- 0.05
+    nboot <- 1000 # bootstrap
+    tr <- 0.2 # group trimmed mean for each quantile
+    nq <- length(qseq)
+    icrit <- round((1-alpha)*nboot) # 95th quantile
+    ilo <- round((alpha/2)*nboot)
+    iup <- nboot - ilo
+    ilo <- ilo + 1
     
     output$DecilesDiff <- renderPlotly({
-        g14 <- HSFData$diffDeciles[[14]]
-        g13 <- HSFData$diffDeciles[[13]]
-        g12 <- HSFData$diffDeciles[[12]]
-        g11 <- HSFData$diffDeciles[[11]]
-        g10 <- HSFData$diffDeciles[[10]]
-        g9 <- HSFData$diffDeciles[[9]]
-        g8 <- HSFData$diffDeciles[[8]]
-        g7 <- HSFData$diffDeciles[[7]]
-        g6 <- HSFData$diffDeciles[[6]]
-        g5 <- HSFData$diffDeciles[[5]]
-        g4 <- HSFData$diffDeciles[[4]]
-        g3 <- HSFData$diffDeciles[[3]]
-        g2 <- HSFData$diffDeciles[[2]]
-        g1 <- HSFData$diffDeciles[[1]]
-        subplot(g14,g13,g12,g11,g10,g9,g8,g7,g6,g5,g4,g3,g2,g1, nrows = 4)
+        for (x in seq(1,length(a))) {
+            subdata = subset(MeasSites$dataSite_long, sid_long == a[x] & sph_long == input$DispHSF)
+            listdat1[[x]] = subdata$siteData
+            listdat2[[x]] = subdata$t1_long
+            lengthDAT[x] = nrow(subdata)
+        }
+        dat1 = stri_list2matrix(listdat1, byrow=TRUE, fill=NA)
+        dat2 = stri_list2matrix(listdat2, byrow=TRUE, fill=NA)
+        dat1 = `dim<-`(as.numeric(dat1), dim(dat1))
+        dat2 = `dim<-`(as.numeric(dat2), dim(dat2))
+        nt = max(lengthDAT)
+        
+        qdiff <- t(apply(dat1, 1, quantile, probs = qseq, type = 8, names = FALSE, na.rm = TRUE) - 
+                       apply(dat2, 1, quantile, probs = qseq, type = 8, names = FALSE, na.rm = TRUE))
+        
+        gptm <- apply(qdiff, 2, mean, trim = tr)
+        
+        df <- tibble(difference = as.vector(qdiff),
+                     quantile = rep(qseq, each = np),
+                     participants = factor(rep(seq(1:np), nq)))
+        
+        df.md <- tibble(difference = gptm,
+                        quantile = qseq)
+        
+        p <- ggplot(df, aes(x = quantile, 
+                            y = difference, 
+                            colour = participants)) + 
+            theme_classic() +
+            geom_line(alpha = 0.5) +
+            geom_abline(slope = 0, intercept = 0) +
+            geom_line(data = df.md, colour = "black", size = 1) +
+            geom_point(data = df.md, colour = "black") +
+            scale_colour_viridis_d(option = "B") +
+            scale_x_continuous(breaks = qseq) +
+            scale_y_continuous(breaks = seq(-200,100,50)) +
+            theme(legend.position = "none",
+                  plot.title = element_text(size=22),
+                  axis.title.x = element_text(size = 18),
+                  axis.text = element_text(size = 16, colour = "black"),
+                  axis.title.y = element_text(size = 18)) + 
+            labs(x = "Deciles", y = "Difference")
+        p
+    })
+    
+    output$BootstrapDiff <- renderPlotly({
+        set.seed(8899)
+        
+        for (x in seq(1,length(a))) {
+            subdata = subset(MeasSites$dataSite_long, sid_long == a[x] & sph_long == input$DispHSF)
+            listdat1[[x]] = subdata$siteData
+            listdat2[[x]] = subdata$t1_long
+            lengthDAT[x] = nrow(subdata)
+        }
+        dat1 = stri_list2matrix(listdat1, byrow=TRUE, fill=NA)
+        dat2 = stri_list2matrix(listdat2, byrow=TRUE, fill=NA)
+        dat1 = `dim<-`(as.numeric(dat1), dim(dat1))
+        dat2 = `dim<-`(as.numeric(dat2), dim(dat2))
+        nt = max(lengthDAT)
+        
+        
+        
+        boot_data1 <- array(data = 0, dim = c(np, nt))
+        boot_data2 <- array(data = 0, dim = c(np, nt))
+        boot_qdiff <- array(data = 0, dim = c(nboot, nq, np))
+        
+        for(B in 1:nboot){
+            
+            # bootstrap participants
+            boot_id <- sample(np, np, replace = TRUE)
+            
+            for(CP in 1:np){ # bootstrap trials
+                boot_data1[CP,] <- sample(dat1[boot_id[CP],], nt, replace = TRUE)
+                boot_data2[CP,] <- sample(dat2[boot_id[CP],], nt, replace = TRUE)
+            }
+            
+            boot_qdiff[B,,] <- apply(boot_data1, 1, quantile, probs = qseq, type = 8, names = FALSE, na.rm = TRUE) - 
+                apply(boot_data2, 1, quantile, probs = qseq, type = 8, names = FALSE, na.rm = TRUE)
+            
+        }
+        
+        boot_tm <- apply(boot_qdiff, c(1,2), mean, trim = tr)
+        sort_boot_tm <- apply(boot_tm, 2, sort)
+        boot_ci <- matrix(data = 0, nrow = 2, ncol = nq)
+        boot_ci[1,] <- sort_boot_tm[ilo,]
+        boot_ci[2,] <- sort_boot_tm[iup,]
+        
+        boot_hdi <- apply(boot_tm, 2, HDInterval::hdi, credMass = 1-alpha)
+        
+        int_to_plot <- boot_ci 
+        int_to_plot <- boot_hdi
+        
+        qdiff <- t(apply(dat1, 1, quantile, probs = qseq, type = 8, names = FALSE, na.rm = TRUE) - 
+                       apply(dat2, 1, quantile, probs = qseq, type = 8, names = FALSE, na.rm = TRUE))
+        
+        gptm <- apply(qdiff, 2, mean, trim = tr)
+        
+        df <- tibble(difference = as.vector(qdiff),
+                     quantile = rep(qseq, each = np),
+                     participants = factor(rep(seq(1:np), nq)))
+        
+        df.md <- tibble(difference = gptm,
+                        quantile = qseq,
+                        ymin = int_to_plot[1,],
+                        ymax = int_to_plot[2,])
+        
+        p <- ggplot(df, aes(x = quantile, 
+                            y = difference, 
+                            colour = participants)) + 
+            theme_classic() +
+            geom_line(alpha = 0.5) +
+            geom_abline(slope = 0, intercept = 0) +
+            geom_line(data = df.md, colour = "black", size = 1) +
+            # geom_point(data = df.md, colour = "black", size = 2) +
+            geom_pointrange(data = df.md, aes(ymin = ymin, ymax = ymax), 
+                            colour = "black", size = 0.75) +
+            scale_colour_viridis_d(option = "B") +
+            scale_x_continuous(breaks = qseq) +
+            # scale_y_continuous(breaks = seq(-500,700,250)) +
+            # coord_cartesian(ylim = c(-500, 700)) +
+            theme(legend.position = "none",
+                  plot.title = element_text(size=22),
+                  axis.title.x = element_text(size = 18),
+                  axis.text = element_text(size = 16, colour = "black"),
+                  axis.title.y = element_text(size = 18)) + 
+            labs(x = "Deciles", y = "Differences")
+        # coord_flip()
+        # ggtitle("Non-Word - Word decile differences")
+        p
+        #p.id <- p
+    })
+    
+    output$BootstrapDensities <- renderPlotly({
+        df <- tibble(boot_samp = as.vector(boot_tm),
+                     quantile = rep(qseq, each = nboot))
+        p <- ggplot(df, aes(x = boot_samp, y = quantile)) +
+            theme_classic() +
+            stat_halfeye(#fill = "orange", 
+                point_interval = mode_hdi,
+                .width = c(0.5, 0.9)
+            ) +
+            geom_vline(xintercept = 0) +
+            scale_y_continuous(breaks = qseq) +
+            theme(plot.title = element_text(size=22),
+                  axis.title.x = element_text(size = 18),
+                  axis.text = element_text(size = 16, colour = "black"),
+                  axis.title.y = element_text(size = 18)) + 
+            xlab("Bootstrap differences") +
+            ylab("Deciles") +
+            #coord_cartesian(xlim = c(-1, 0.1)) +
+            coord_flip()
+        p
     })
 
-    #output$BootstrapDensities <- renderPlotly({
-    #    HSFData <- hierarchical_shift_function(dataSites, input$DispHSF)
-    #    p <- ggplot(HSFData$densitiesBootstrap, aes(x = boot_samp, y = quantile)) +
-    #        theme_classic() +
-    #        stat_halfeye(#fill = "orange", 
-    #            point_interval = mode_hdi,
-    #            .width = c(0.5, 0.9)
-    #        ) +
-    #        geom_vline(xintercept = 0) +
-    #        scale_y_continuous(breaks = seq(0.1,0.9,0.1)) +
-    #        theme(plot.title = element_text(size=22),
-    #              axis.title.x = element_text(size = 18),
-    #              axis.text = element_text(size = 16, colour = "black"),
-    #              axis.title.y = element_text(size = 18)) + 
-    #        xlab("Bootstrap differences") +
-    #        ylab("Deciles") +
-    #        #coord_cartesian(xlim = c(-1, 0.1)) +
-    #        coord_flip()
-    #    p
-    #})
-
-    #TAB 5
+    #TAB 4
     output$Disp4Site <- renderPlotly({
         if (input$selectCompSite == "Montreal"){
             RefVSMeas = measuredT1_against_referenceT1(scans = Montreal)
@@ -834,7 +938,7 @@ server <- function(input, output) {
     #    ggplotly(multPlot)
     #})
     
-    #TAB 6
+    #TAB 5
     output$sdFilteredSites <- renderPlotly({
         sdFiltered_colors <- setNames(rainbow(nrow(sdFilteredSites$stdData)), sdFilteredSites$stdData$ID_Site)
         plot_ly(sdFilteredSites$stdData, x = ~reference, y = ~stdValues/reference, split = ~ID_Site, color = ~ID_Site, colors = sdFiltered_colors) %>%
@@ -850,7 +954,7 @@ server <- function(input, output) {
                    legend = list(title = list(text = "<b>Site ID</b>")))
     })
 
-    #TAB 7
+    #TAB 6
     #output$boxPlotLME <- renderPlotly({
     #    p <- ggplot(data = filter(sitesLMEM$dataLME, sid %in% input$boxPlotSite)) +
     #        geom_boxplot(aes(x = sphere, y = dataSphere, fill = factor(sphere))) +
